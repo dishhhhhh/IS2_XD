05e6261825ca6b47dab073e4e0b3e62cdc36a2c9
dishh <unaostra@gmail.com>
2026-06-04 21:15:09 -0500

    Initial commit

---
+++ src/main/java/com/example/is2_xd/Is2XdApplication.java
@@ -0,0 +1,14 @@
+package com.example.is2_xd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is2XdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Is2XdApplication.class, args);
+        System.out.println("hola");
+    }
+
+}
---
+++ src/test/java/com/example/is2_xd/Is2XdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.is2_xd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is2XdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
